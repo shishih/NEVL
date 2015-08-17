31e4b6a15671021d2fb285d2ec960e6842f3ad0e
shishih <agnesyao1110@gmail.com>
2015-08-17 14:49:27 -0400

    add json

---
+++ imdbKey.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import json
+import urllib, urllib2
+
+
+def get_movList():
+    url='http://www.imdb.com/calendar/?ref_=nv_mv_cal_5'
+
+def movie2json(movName,movId,movDate)
+    data = {
+        'movName':movName,
+        'movId':movId,
+        'movDate':movDate,
+    }
+    json_str = json.dumps(data)
+    return json_str
+
+
+
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
